d8d5dfaeefb2805e9490cf9b46916be06214a22a
cutrain <454615501@qq.com>
2017-12-19 16:56:16 +0800

    feat: finish structure building

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 HOST = '0.0.0.0'
-PORT = 8080
+PORT = 5000
+SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:123@106.14.132.219:3306/pionner'
 DEBUG = True
 
 SECRET_KEY = 'pioneer'
--- pioneer/models.py
+++ pioneer/models.py
@@ -1,3 +1,58 @@
-from flask_login import UserMixin
-class User(UserMixin):
-    pass
+from . import db
+
+
+class Users(db.Model):
+    __tablename__ = 'Users'
+    userId = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    password = db.Column(db.String(30), nullable=False)
+    role = db.Column(db.Integer, nullable=False)
+    token = db.Column(db.String(256))
+    mail = db.Column(db.String(50))
+    iconlink = db.Column(db.String(256))
+    posts = db.relationship('Posts', backref='user', lazy=True)
+    replies = db.relationship('Replies', backref='user', lazy=True)
+
+    def __repr__(self):
+        return "<Users %r>" % self.username
+
+
+class Posts(db.Model):
+    __tablename__ = 'Posts'
+    postId = db.Column(db.Integer, primary_key=True)
+    postTitle = db.Column(db.String(50), nullable=False)
+    postTime = db.Column(db.DateTime, nullable=False)
+    forumName = db.Column(db.String(50), nullable=False)
+    userId = db.Column(db.Integer, db.ForeignKey('Users.userId'))
+
+    def __repr__(self):
+        return '<Posts %r>' % self.postTitle
+
+
+class Replies(db.Model):
+    __tablename__ = 'Replies'
+    replyId = db.Column(db.Integer, primary_key=True)
+    replyToId = db.Column(db.Integer, nullable=False)
+    replyTime = db.Column(db.DateTime, nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    floor = db.Column(db.Integer, nullable=False, unique=True)
+    postId = db.Column(db.Integer, db.ForeignKey('Posts.postId'), nullable=False)
+    userId = db.Column(db.Integer, db.ForeignKey('Users.userId'), nullable=False)
+
+    def __repr__(self):
+        return '<Replies %r>' % self.content
+
+
+Likes = db.Table(
+    'Likes',
+    db.Column('userId', db.Integer, db.ForeignKey('Users.userId'), primary_key=True),
+    db.Column('replyId', db.Integer, db.ForeignKey('Replies.replyId'), primary_key=True),
+)
+
+
+Favorates = db.Table(
+    'Favorates',
+    db.Column('userId', db.Integer, db.ForeignKey('Users.userId'), primary_key=True),
+    db.Column('PostId', db.Integer, db.ForeignKey('Posts.postId'), primary_key=True),
+)
+
