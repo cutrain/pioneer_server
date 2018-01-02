5d455e79ca8c0a2f3d2790e33462fcc3f867a61b
cutrain <454615501@qq.com>
2018-01-02 23:58:55 +0800

    fix: login auth

--- pioneer/__init__.py
+++ pioneer/__init__.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask.ext.httpauth import HTTPBasicAuth
+from flask.ext.httpauth import HTTPTokenAuth
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -9,7 +9,7 @@ app.secret_key = app.config['SECRET_KEY']
 
 db = SQLAlchemy(app)
 
-auth = HTTPBasicAuth()
+auth = HTTPTokenAuth(scheme='Token')
 
 import pioneer.models
 import pioneer.views
