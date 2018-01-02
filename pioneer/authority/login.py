40a61c7f5f71103a5c95a52f01234ee2cb2c9b15
cutrain <454615501@qq.com>
2018-01-02 17:03:07 +0800

    fix: fix requires & json format

--- pioneer/authority/login.py
+++ pioneer/authority/login.py
@@ -2,14 +2,17 @@ from .. import app, db, auth
 from ..models import Users
 from flask import request, jsonify
 import time
+import json
 
 
 @app.route('/login', methods=['POST'])
 def login():
     # TODO: login & modify database
+    inform = ''
     try:
-        username = request.form.get('username')
-        password = request.form.get('password')
+        data = json.loads(request.get_data())
+        username = data['username']
+        password = data['password']
         user = Users.query.filter_by(username=username).first()
         if user is not None and password == user.password:
             user.token = user.generate_token()
@@ -21,12 +24,15 @@ def login():
                 'state':200,
                 'token':user.token
             })
+        else:
+            inform = 'password error'
     except KeyError:
-        pass
+        inform = 'username or password is not specified'
     return jsonify({
         'id':0,
         'name':'',
         'role':0,
         'state':404,
         'token':'',
+        'info': inform
     })
--- pioneer/authority/register.py
+++ pioneer/authority/register.py
@@ -1,17 +1,20 @@
 from .. import app, db
 from flask import request, abort, jsonify
 from ..models import Users
+import json
 
 @app.route('/register', methods=['POST'])
 def register():
     # TODO: register
-    username = request.form.get('username')
-    password = request.form.get('password')
-    mail = request.form.get('mail')
-    if any(i is None for i in [username, password, mail]):
-        abort(400)
+    data = json.loads(request.get_data())
+    try:
+        username = data['username']
+        password = data['password']
+        mail = data['mail']
+    except KeyError:
+        return jsonify({'state':400, 'info':'lack of [username, password, mail]'})
     if Users.query.filter_by(username=username).first() is not None:
-        abort(400)
+        return jsonify({'state':400, 'info':'username already used'})
     user = Users(username=username, password=password, mail=mail, role=0)
     db.session.add(user)
     db.session.commit()
