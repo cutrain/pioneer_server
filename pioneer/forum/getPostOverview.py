64c898d33f5c202dcec2cbc038eefb385e7fc279
ZhiQin Zhan <zqzhan14@fudan.edu.cn>
2018-01-05 00:54:56 +0800

    edit pioneer/forum/getPostOverview.py

--- pioneer/forum/getPostOverview.py
+++ pioneer/forum/getPostOverview.py
@@ -1,9 +1,22 @@
 from .. import app, db, auth
 from ..models import Users, Posts, Replies, Likes, Favorates
-
+from flask import request, jsonify
+import json
+from .getForumList import getPostInfo
 
 @app.route('/forum/getPostOverview', methods=['POST'])
 @auth.login_required
 def getPostOverview():
-    # TODO
-    pass
+    try:
+        data = json.loads(request.get_data().decode('utf-8'))
+        postId = data['postId']
+        post = Posts.query.filter_by(postId=postId).one()
+        return jsonify({
+            'stateCode': 200,
+            'postList': [getPostInfo(post)]
+        }) 
+    except KeyError:
+        pass
+    return jsonify({
+        'stateCode': 404
+    })
