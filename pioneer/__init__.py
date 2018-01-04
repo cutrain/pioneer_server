a9d01ea32ab74d9cd30ad8752bd6e8f720d61292
cutrain <454615501@qq.com>
2018-01-04 21:16:36 +0800

    feat: flask-cors

--- pioneer/__init__.py
+++ pioneer/__init__.py
@@ -2,11 +2,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_httpauth import HTTPTokenAuth
+from flask_cors import *
 
 app = Flask(__name__)
 app.config.from_object('config')
 app.secret_key = app.config['SECRET_KEY']
 
+CORS(app, supports_credentials=True)
 db = SQLAlchemy(app)
 
 auth = HTTPTokenAuth(scheme='Token')
