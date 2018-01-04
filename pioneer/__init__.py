e95fb76ad95f8e93e51c5ab3a1cec319622c47f6
cutrain <454615501@qq.com>
2018-01-04 20:12:38 +0800

    test: apply all

--- pioneer/__init__.py
+++ pioneer/__init__.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask.ext.httpauth import HTTPTokenAuth
+from flask_httpauth import HTTPTokenAuth
 
 app = Flask(__name__)
 app.config.from_object('config')
