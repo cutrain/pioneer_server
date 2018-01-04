ef50985f2787d6fa46fe7c515d436e1078cae31c
ZhiQin Zhan <zqzhan14@fudan.edu.cn>
2018-01-05 00:35:48 +0800

    Edit pioneer/forum/getForumList.py

--- pioneer/forum/getForumList.py
+++ pioneer/forum/getForumList.py
@@ -1,9 +1,31 @@
 from .. import app, db, auth
 from ..models import Users, Posts, Replies, Likes, Favorates
-
+from flask import request, jsonify
+import json
 
 @app.route('/forum/getForumList', methods=['POST'])
 @auth.login_required
 def getForumList():
-    # TODO
-    pass
+    try:
+        data = json.loads(request.get_data().decode('utf-8'))
+        forumName = data['forumName']
+        posts = Posts.query.filter_by(forumName=forumName).all()
+        return jsonify({
+            'stateCode': 200,
+            'postList': [getPostInfo(post) for post in posts]
+        }) 
+    except KeyError:
+        pass
+    return jsonify({
+        'stateCode': 404
+    })
+
+def getPostInfo(post):
+    info = {}
+    info['postId'] = post.postId
+    info['postTitle'] = post.postTitle
+    info['postUser'] = Users.query.filter_by(userId=post.userId).one().username
+    replies = Replies.query.filter_by(postId=post.postId).all()
+    info['lastReplyTime'] = max(reply.replyTime for reply in replies)
+    info['replyNum'] = len(replies)
+    return info
