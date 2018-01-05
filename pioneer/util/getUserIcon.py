ec9c22d1b3796c1a6e240eba6e5cef8bf961c5ee
cutrain <454615501@qq.com>
2018-01-05 13:07:38 +0800

    feat: pictures

--- pioneer/util/getUserIcon.py
+++ pioneer/util/getUserIcon.py
@@ -1,11 +1,13 @@
 from .. import app, db, auth
 from ..models import Users, Posts, Replies, Likes, Favorates
-from flask import request, jsonify, g
+from flask import request, jsonify, g, send_from_directory
 import json
 
 @app.route('/util/getUserIcon', methods=['POST'])
 @auth.login_required
 def getUserIcon():
+    return jsonify({
+        'iconUrl':'/util/getPic/1'})
     try:
         data = json.loads(request.get_data().decode('utf-8'))
         typ = data['type']
@@ -25,3 +27,7 @@ def getUserIcon():
             'iconUrl':'',
             'info':'type or userInfo doesn\'t specify'
         })
+
+@app.route('/util/getPic/<int:num>', methods=['GET'])
+def getPic(num):
+    return send_from_directory('pics', 'icon.jpg', as_attachment=True)
